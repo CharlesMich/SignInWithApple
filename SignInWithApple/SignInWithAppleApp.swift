fdee5781c844d8488a2e7c8d38e87dd61aa93c0a
CharlesMich <charleedongo@hotmail.com>
2025-04-02 16:13:41 -0500

    Initial Commit

---
+++ SignInWithApple/SignInWithAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignInWithAppleApp.swift
+//  SignInWithApple
+//
+//  Created by Charles Michael on 4/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SignInWithAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
